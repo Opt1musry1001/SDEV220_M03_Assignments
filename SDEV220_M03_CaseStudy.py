94f3cdbab49d52b9ef48edbda9b2d5c355095ed9
Opt1musry1001 <rmoore318@ivytech.edu>
2023-09-09 22:36:16 -0400

    Added more header comments and fixed inheritance - RM

--- SDEV220_M03_CaseStudy.py
+++ SDEV220_M03_CaseStudy.py
@@ -1,17 +1,27 @@
 # SDEV220_M03_CaseStudy.py
 # Author: Ryan Moore
 # Date: 9/9/23
-# Purpose: Creates a super class and a class that inherits its values.
-# Allows for user input, saves to classes, and then outputs the data.
+# 
+# Purpose: Creates a super class and a class that inherits its values,
+# and allows for user input, saves to classes, and then outputs the data.
+# 
+# Variables:
+# The vehicleType, year, make, model, doors, and roof variables all hold their respective information about a vehicle.
+# The auto variable is an object of type Automobile that holds the user's given vehicle data.
+# The Vehicle class has a variable vehicleType, which stores the type of vehicle.
+# The Automobile class inherits vehicleType from the Vehicle class and also contains year, make, model, doors,
+# and roof variables to contain vehicle information.
+
 
 # Create Vehicle class
 class Vehicle():
-    vehicleType = ""
+    def __init__(self, vehicleType):
+        self.vehicleType = vehicleType
 
 # Create Automobile class, inheriting from Vehicle class
 class Automobile(Vehicle):
     def __init__(self, vehicleType, year, make, model, doors, roof):
-        self.vehicleType = vehicleType
+        super().__init__(vehicleType)   # Calls parent class initialize function.
         self.year = year
         self.make = make
         self.model = model
