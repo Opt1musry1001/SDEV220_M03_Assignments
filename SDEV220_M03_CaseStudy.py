352a1badf3843aa59db0ecdc7860f4e7be847dec
Opt1musry1001 <rmoore318@ivytech.edu>
2023-09-09 22:20:17 -0400

    Added input validation - RM

--- SDEV220_M03_CaseStudy.py
+++ SDEV220_M03_CaseStudy.py
@@ -18,13 +18,23 @@ class Automobile(Vehicle):
         self.doors = doors
         self.roof = roof
 
+# Initialize necessary variables.
+doors = ""
+roof = ""
+
 # Get input from user.
 vehicleType = input("Enter the vehicle's type: ")
 year = input("Enter the vehicle's year: ")
 make = input("Enter the vehicle's make: ")
 model = input("Enter the vehicle's model: ")
-doors = input("Enter the vehicle's number of doors: ")
-roof = input("Enter the vehicle's roof type: ")
+while (doors != "2") and (doors != "4"):            # Check to see if number of doors is valid.
+    doors = input("Enter the vehicle's number of doors: ")
+    if (doors != "2") and (doors != "4"):
+        print("Error: Please enter 2 or 4.")
+while (roof != "sunroof") and (roof != "solid"):    # Check to see if roof type is valid.
+    roof = input("Enter the vehicle's roof type: ")
+    if (roof != "sunroof") and (roof != "solid"):
+        print("Error: Please enter solid or sunroof:")
 
 # Initialize Automobile object using given data.
 auto = Automobile(vehicleType, year, make, model, doors, roof)
